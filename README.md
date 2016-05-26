cbf707288cd8e47b522c00d82a19c5ad3bc31fa2
Tim Su <tim.y.su@gmail.com>
2016-05-26 17:08:30 +1000

    Added invoice display

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # A chrome plugin for MYOB Essentials
 
 ## Set up
+
+
+## Reference
+http://enehana.nohea.com/general/using-handlebars-js-templates-as-precompiled-js-files/
--- dist/templates.js
+++ dist/templates.js
@@ -1,9 +1,11 @@
 this["ts"] = this["ts"] || {};
 this["ts"]["templates"] = this["ts"]["templates"] || {};
 this["ts"]["templates"]["statsContainer"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    var helper;
+    var stack1, helper, alias1=this.escapeExpression;
 
-  return "<div class='stats-container'>\n    <p>\n    <h4>"
-    + this.escapeExpression(((helper = (helper = helpers.count || (depth0 != null ? depth0.count : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"count","hash":{},"data":data}) : helper)))
-    + "</h4> Invoices issued.\"\n    </p>\n</div>";
+  return "<div class='stats-container'>\n    <span class='badge blue'>"
+    + alias1(((helper = (helper = helpers.count || (depth0 != null ? depth0.count : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"count","hash":{},"data":data}) : helper)))
+    + "</span> Invoices issued\n    <span class='badge red'>$"
+    + alias1(this.lambda(((stack1 = (depth0 != null ? depth0.due : depth0)) != null ? stack1.value : stack1), depth0))
+    + "</span> worth of Invoices due\n</div>";
 },"useData":true});
